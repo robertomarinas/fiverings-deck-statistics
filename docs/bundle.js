62f135852a0a2ed9321fc256df7f25180c7e79f1
Bobby Marinas <robertomartin.marinas@gmail.com>
2017-12-29 23:06:00 +0800

    updated app modal

--- docs/bundle.js
+++ docs/bundle.js
@@ -26475,7 +26475,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var customStyles = {
 	overlay: {
 		position: 'fixed',
-		zIndex: 9,
+		zIndex: 10,
 		top: 0,
 		left: 0,
 		right: 0,
@@ -26485,12 +26485,12 @@ var customStyles = {
 	content: {
 		// width				  : '200px',
 		maxWidth: 700,
-		maxHeight: 700,
+		maxHeight: 900,
 		margin: '0 auto',
-		top: '10%',
+		top: '12%',
+		bottom: '12%',
 		left: '10%',
-		right: '10%',
-		bottom: '10%'
+		right: '10%'
 		// marginRight           : '-50%',
 		// transform             : 'translate(-50%, -50%)'
 	}
